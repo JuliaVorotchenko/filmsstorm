484cecd4259dbf63e0649ba847ed9e7508937899
JuliaVorotchenko <juliavorotchenko@gmail.com>
2020-01-06 23:43:40 +0200

    Added MVC + C pattern

---
+++ filmsstorm/Source/Patterns/MVCC/Coordinator.swift
@@ -0,0 +1,44 @@
+//
+//  Coordinator.swift
+//  filmsstorm
+//
+//  Created by Юлия Воротченко on 06.01.2020.
+//  Copyright © 2020 Alexander Andriushchenko. All rights reserved.
+//
+
+import UIKit
+
+/**
+ MVC+C is the base pattern on the project. Main actors are:
+ - Model - Data container, persistance object;
+ - View - Object to represent data on UI. You must inherite Cotroller type to use it as View.
+ - Controller - Object, which acts as the intermediary between the application's view objects and its model objects.
+ - Coordinator - Entity, which allows to manage all controllers and sub-coordinators.
+ 
+ Coordinator`s roles:
+ - Incapsulates dependencies;
+ - Provides dependency injection role;
+ - Init controllers;
+ - Navigate controllers (show and hide instack, modally and in any other way);
+ - Subscribes on controller`s events and react on events;
+ - Show or hide sub-coordinators if needed.
+ 
+ Coordinator CAN NOT:
+ - Save any controller as property;
+ */
+protocol Coordinator: AnyObject {
+    var childCoordinators: [Coordinator] { get set }
+    var navigationController: UINavigationController { get }
+    func start()
+}
+
+extension Coordinator {
+    func addCoordinator(_ childCoordinator: Coordinator) {
+        self.childCoordinators.append(childCoordinator)
+    }
+    
+    func removeCoordinator(_ childCoordinator: Coordinator) {
+        self.childCoordinators = self.childCoordinators.filter { $0 !== childCoordinator }
+    }
+    
+}
