f40e94a6383ca306d90a145c5bb584ed53dc4120
JuliaVorotchenko <juliavorotchenko@gmail.com>
2020-01-22 18:01:10 +0200

    Preliminary alerts implementattion

---
+++ filmsstorm/Source/ProjectExtensions/UIViewControllerExtension.swift
@@ -0,0 +1,52 @@
+//
+//  UIViewControllerextension.swift
+//  filmsstorm
+//
+//  Created by Юлия Воротченко on 22.01.2020.
+//  Copyright © 2020 Alexander Andriushchenko. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    struct  TextConstants {
+        static let close = "Close"
+        static let cancel = "Cancel"
+        static let serverError = "Server Error"
+    }
+    
+    private typealias Text = TextConstants
+    
+    func showAlert(title: String?,
+                   message: String? = nil,
+                   preferredStyle: UIAlertController.Style = .alert,
+                   actions: [UIAlertAction]? = [UIAlertAction(title: Text.close,
+                                                              style: .destructive,
+                                                              handler: nil)]) {
+        let alertController = UIAlertController(title: title,
+                                                message: message,
+                                                preferredStyle: preferredStyle)
+        actions?.forEach { alertController.addAction($0) }
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    func showErrorAlert(_ title: String?, error: Error?) {
+        self.showAlert(title: title, message: error?.localizedDescription)
+    }
+    
+    func showServerErrorAlert(_ error: NetworkError?) {
+        self.showAlert(title: Text.serverError, message: error?.stringDescription)
+    }
+    
+    func showAlertWithAction(title: String?,
+                             message: String?,
+                             actionTitle: String?,
+                             action: ((UIAlertAction) -> Void)?) {
+        let alertAction = UIAlertAction(title: actionTitle,
+                                        style: .default,
+                                        handler: action)
+        self.showAlert(title: title,
+                       message: message,
+                       actions: [alertAction])
+    }
+}
