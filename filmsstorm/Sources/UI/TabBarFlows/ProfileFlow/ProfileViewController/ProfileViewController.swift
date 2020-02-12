6bb50eb6e3fd7e64edad10d9885bb454786ecbc6
JuliaVorotchenko <juliavorotchenko@gmail.com>
2020-02-13 01:36:19 +0200

    Add diffable data source

--- filmsstorm/Sources/UI/TabBarFlows/ProfileFlow/ProfileViewController/ProfileViewController.swift
+++ filmsstorm/Sources/UI/TabBarFlows/ProfileFlow/ProfileViewController/ProfileViewController.swift
@@ -26,6 +26,13 @@ class ProfileViewController: UIViewController, Controller, ActivityViewPresenter
         case main
     }
     
+    enum Item: Hashable {
+        case profile(UserModel)
+        case imageQuality
+        case about(ActionCellModel)
+        case logout(ActionCellModel)
+    }
+    
     // MARK: - Public Properties
     
     let loadingView = ActivityView()
@@ -35,6 +42,8 @@ class ProfileViewController: UIViewController, Controller, ActivityViewPresenter
     
     private let networking: NetworkManager
     private var user: UserModel?
+    private var items: [Item] = []
+    private lazy var dataSource = self.diffableDataSource()
     
     // MARK: - Init & deinit
     
@@ -52,15 +61,16 @@ class ProfileViewController: UIViewController, Controller, ActivityViewPresenter
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.tabBarController?.tabBar.isHidden = false
-        self.setTableVievDelegate()
-        self.regiterCells()
+        
+        self.setupTableView()
+        
         self.getUserDetails()
+        // self.createItems()
     }
     
     // MARK: - Private methods
     
-    private func logout() {
+    private func onLogout() {
         self.showActivity()
         let sessionID = UserDefaultsContainer.session
         self.networking.logout(sessionID: sessionID) { [weak self] result in
@@ -82,65 +92,67 @@ class ProfileViewController: UIViewController, Controller, ActivityViewPresenter
             switch result {
             case .success(let model):
                 self?.user = model
-                self?.rootView?.tableView.reloadData()
+                self?.createItems()
             case .failure(let error):
                 self?.eventHandler?(.error(.networkingError(error)))
             }
         }
     }
-}
-
-extension ProfileViewController: UITableViewDataSource {
     
-    // MARK: - Table view data source
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 4
+    private func createItems() {
+        guard let userModel = self.user else { return }
+        let aboutCellModel = ActionCellModel(name: "About us", image: nil, action: self.onAbout)
+        let logoutCellModel = ActionCellModel(name: "Logout", image: nil, action: self.onLogout)
+        self.items = [.profile(userModel), .imageQuality, .about(aboutCellModel), .logout(logoutCellModel)]
+        self.update(sections: Section.allCases, items: self.items)
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        var cell = UITableViewCell()
-        
-        if indexPath.row == 0 {
-            let cell0: AvatarViewCell = tableView.dequeueReusableCell(AvatarViewCell.self, for: indexPath)
-            guard let userModel = self.user else { return UITableViewCell() }
-            cell0.fill(model: userModel)
-            cell = cell0
-        }
-        
-        if indexPath.row == 1 {
-            let cell1: QualitySettingViewCell = tableView.dequeueReusableCell(QualitySettingViewCell.self, for: indexPath)
-            cell = cell1
-        }
-        
-        if indexPath.row == 2 {
-            let cell2: AboutViewCell = tableView.dequeueReusableCell(AboutViewCell.self, for: indexPath)
-            cell2.aboutTappedAction = { [weak self] in
-                self?.eventHandler?(.about)
-            }
-            cell = cell2
-        }
-        
-        if indexPath.row == 3 {
-            let cell3: LogoutViewCell  = tableView.dequeueReusableCell(LogoutViewCell.self, for: indexPath)
-            cell3.logoutTappedAction = { [weak self] in
-                self?.logout()
-            }
-            cell = cell3
-        }
-        
-        return cell
+    private func onAbout() {
+        self.eventHandler?(.about)
     }
     
-    private func setTableVievDelegate() {
-        self.rootView?.tableView.dataSource = self
+    private func update(sections: [Section], items: [Item]) {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
+        snapshot.appendSections(sections)
+        snapshot.appendItems(items)
+        self.dataSource?.apply(snapshot)
+        
     }
     
-    private func regiterCells() {
+    private func setupTableView() {
         let tableView = self.rootView?.tableView
         tableView?.register(AvatarViewCell.self)
         tableView?.register(QualitySettingViewCell.self)
-        tableView?.register(AboutViewCell.self)
-        tableView?.register(LogoutViewCell.self)
+        tableView?.register(ActionViewCell.self)
+        tableView?.dataSource = self.dataSource
     }
+    
+    private func diffableDataSource() -> UITableViewDiffableDataSource<Section, Item>? {
+        
+        return self.rootView.map {
+            UITableViewDiffableDataSource(tableView: $0.tableView) { (tableView, indexPath, items) -> UITableViewCell? in
+                var cell = UITableViewCell()
+                
+                switch items {
+                case .profile(let model):
+                    let avatarCell: AvatarViewCell = tableView.dequeueReusableCell(AvatarViewCell.self, for: indexPath)
+                    avatarCell.fill(model: model)
+                    cell = avatarCell
+                case .imageQuality:
+                    let qualityCell: QualitySettingViewCell = tableView.dequeueReusableCell(QualitySettingViewCell.self, for: indexPath)
+                    cell = qualityCell
+                case .about(let model):
+                    let aboutCell: ActionViewCell = tableView.dequeueReusableCell(ActionViewCell.self, for: indexPath)
+                    aboutCell.fill(with: model)
+                    cell = aboutCell
+                case .logout(let model):
+                    let logoutCell: ActionViewCell = tableView.dequeueReusableCell(ActionViewCell.self, for: indexPath)
+                    logoutCell.fill(with: model)
+                    cell = logoutCell
+                }
+                return cell
+            }
+        }
+    }
+    
 }
