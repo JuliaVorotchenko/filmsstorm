7eaa82d2e1c3de7927e5f29514ce9d6f81dbc7b5
JuliaVorotchenko <juliavorotchenko@gmail.com>
2020-03-12 23:33:12 +0200

    Add preliminary UICollectionViewLayout

--- filmsstorm/Sources/UI/TabBarFlows/DiscoverFlow/MediaItemViewController/MediaItemViewController.swift
+++ filmsstorm/Sources/UI/TabBarFlows/DiscoverFlow/MediaItemViewController/MediaItemViewController.swift
@@ -15,10 +15,11 @@ class MediaItemViewController<T: MediaItemPresenter>: UIViewController, Controll
     typealias RootViewType = MediaItemView
     typealias Service = T
     
-    enum Section: CaseIterable {
+    enum Section: Int, CaseIterable {
         case main
         case overview
         case similars
+        case actors
     }
     
     // MARK: - Private properties
@@ -47,7 +48,13 @@ class MediaItemViewController<T: MediaItemPresenter>: UIViewController, Controll
     override func viewDidLoad() {
         super.viewDidLoad()
         self.setupNavigationView()
+        self.cellRegister()
+        self.setCollectionView()
     }
+    
+    // MARK: - Private methods
+
+
     
    private func setupNavigationView() {
     
@@ -63,6 +70,70 @@ class MediaItemViewController<T: MediaItemPresenter>: UIViewController, Controll
         self.rootView?.collectionView.register(ImageViewCell.self)
         self.rootView?.collectionView.register(ItemDescriptionViewCell.self)
         self.rootView?.collectionView.register(ItemOverviewViewCell.self)
+    }
+    
+    private func setCollectionView() {
+        let layout = self.createLayout()
+           self.rootView?.collectionView.setCollectionViewLayout(layout, animated: true)
+       }
+    
+    private func createLayout() -> UICollectionViewLayout {
+        return UICollectionViewCompositionalLayout { sectionNumber, env -> NSCollectionLayoutSection? in
+            switch Section(rawValue: sectionNumber) {
+            case .main:
+                return self.mainSection()
+            case .overview:
+                return self.overviewSection()
+            case .similars:
+                return self.similarsSection()
+            case .actors:
+                return self.similarsSection()
+            case .none:
+               return nil
+            }
+        }
+    }
+    
+    private func mainSection() -> NSCollectionLayoutSection {
+        let sectionWidth = self.view.bounds.width
+        let itemSize = NSCollectionLayoutSize(widthDimension: .absolute(sectionWidth),
+                                              heightDimension: .absolute(248))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .absolute(sectionWidth),
+                                               heightDimension: .absolute(248))
+        let group = NSCollectionLayoutGroup(layoutSize: groupSize)
+        
+        return NSCollectionLayoutSection(group: group)
+    }
+    
+    private func overviewSection() -> NSCollectionLayoutSection {
+        let sectionWidth = self.view.bounds.width
+        let itemSize = NSCollectionLayoutSize(widthDimension: .absolute(sectionWidth),
+                                                     heightDimension: .absolute(170))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .absolute(sectionWidth),
+                                               heightDimension: .absolute(248))
+        let group = NSCollectionLayoutGroup(layoutSize: groupSize)
+        
+        return NSCollectionLayoutSection(group: group)
+    }
+    
+    private func similarsSection() -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.25),
+                                             heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = NSDirectionalEdgeInsets(top: 8, leading: 8, bottom: 8, trailing: 8)
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                               heightDimension: .fractionalHeight(0.3))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize,
+                                                         subitem: item, count: 3)
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .continuous
+        return section
     }
 
 }
