773eb64fd27ba4b33637a28d4cce2fc3dc93f4e6
JuliaVorotchenko <juliavorotchenko@gmail.com>
2020-05-04 13:09:25 +0300

    Update AuhorizaionVC layout

--- filmsstorm/Sources/UI/LoginFlow/AuthorizationViewController/AuthorizationView.swift
+++ filmsstorm/Sources/UI/LoginFlow/AuthorizationViewController/AuthorizationView.swift
@@ -9,7 +9,21 @@
 import UIKit
 
 class AuthorizationView: UIView {
-
+    
     @IBOutlet var usernameTextField: UITextField?
     @IBOutlet var passwordTextField: UITextField?
+    @IBOutlet var loginButton: UIButton?
+    @IBOutlet var signUpButon: UIButton?
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.setUI()
+    }
+    
+    private func setUI() {
+        self.usernameTextField?.setIcon(UIImage(named: "usernameIcon")!)
+        self.passwordTextField?.setIcon(UIImage(named: "passwordIcon")!)
+        self.loginButton?.addShadow()
+        self.signUpButon?.addShadow()
+    }
 }
