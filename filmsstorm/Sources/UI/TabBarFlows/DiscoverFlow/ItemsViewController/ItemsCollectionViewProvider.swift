0bd12cbca1b977454ab2372903677467c4eb3c47
JuliaVorotchenko <juliavorotchenko@gmail.com>
2020-05-21 15:36:40 +0300

    Reffactor ItemsViewController with collection view provider

---
+++ filmsstorm/Sources/UI/TabBarFlows/DiscoverFlow/ItemsViewController/ItemsCollectionViewProvider.swift
@@ -0,0 +1,15 @@
+//
+//  ItemsCollectionViewProvider.swift
+//  filmsstorm
+//
+//  Created by Юлия Воротченко on 21.05.2020.
+//  Copyright © 2020 Alexander Andriushchenko. All rights reserved.
+//
+
+import UIKit
+
+class ItemsCollectionViewProvider: NSObject, UICollectionViewDelegate {
+    
+   
+    
+}
