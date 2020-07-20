08ee3e3f76e8c0d473da54fcae57d7b7072b7b35
JuliaVorotchenko <juliavorotchenko@gmail.com>
2020-07-20 22:21:36 +0300

    Add core data manager

---
+++ filmsstorm/Sources/Services/CoreDataManager/CoreDataManager.swift
@@ -0,0 +1,39 @@
+//
+//  CoreDataManager.swift
+//  filmsstorm
+//
+//  Created by Юлия Воротченко on 20.07.2020.
+//  Copyright © 2020 Alexander Andriushchenko. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreData
+
+class CoreDataManager {
+    static let shared = CoreDataManager()
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "filmsstorm")
+        container.loadPersistentStores { _, error in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        return container
+    }()
+    
+    private init() {}
+    
+    func saveContext () {
+           let context = persistentContainer.viewContext
+           if context.hasChanges {
+               do {
+                   try context.save()
+               } catch {
+                   let nserror = error as NSError
+                   fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+               }
+           }
+       }
+}
